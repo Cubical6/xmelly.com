369b8adb1950c0f098ea210d4a9146636bd95327
Nuno Maduro <enunomaduro@gmail.com>
2025-04-13 17:13:19 +0100

    Adjusts sponsors

--- resources/views/components/docs/sponsors.blade.php
+++ resources/views/components/docs/sponsors.blade.php
@@ -35,11 +35,6 @@
                         </a>
                     </li>
                     <li class="block">
-                        <a href="https://larajobs.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
-                            <img src="/www/assets/sponsors/larajobs.svg" class="w-auto h-10" />
-                        </a>
-                    </li>
-                    <li class="block">
                         <a href="https://coderabbit.ai/?ref=pestphp" target="_blank" rel="noopener" class="table">
                             <img src="/www/assets/sponsors/coderabbit.svg" class="w-auto h-10" />
                         </a>
@@ -49,17 +44,21 @@
                             <img src="/www/assets/sponsors/brokerchooser.svg" class="w-auto h-12" />
                         </a>
                     </li>
-
                     <li class="block">
-                        <a href="https://forge.laravel.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
-                            <img src="/www/assets/sponsors/forge.svg" class="w-auto h-6" />
+                        <a href="https://nativephp.com/mobile?ref=pestphp.com" target="_blank" rel="noopener" class="table">
+                            <img src="/www/assets/sponsors/native-php.svg" class="w-auto h-12" />
                         </a>
                     </li>
                 </ul>
                 <ul class="pb-8 border-b border-gray-200 dark:border-gray-800 drop-shadow md:gap-3 lg:px-0 flex flex-wrap items-center justify-center gap-3 px-6 mt-6">
                     <li class="md:block hidden w-full h-0 -mt-12"></li>
+                        <a href="https://forge.laravel.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
+                            <img src="/www/assets/sponsors/forge.svg" class="w-auto h-5" />
+                        </a>
+                    </li>
+                    <li class="md:block hidden w-full h-0 -mt-12"></li>
                         <a href="https://spatie.be/?ref=pestphp" target="_blank" rel="noopener" class="table">
-                            <img src="/www/assets/sponsors/spatie.svg" class="w-auto h-10" />
+                            <img src="/www/assets/sponsors/spatie.svg" class="w-auto h-7" />
                         </a>
                     </li>
                     <li class="md:block hidden w-full h-0 -mt-12"></li>
@@ -95,12 +94,6 @@
                     <li class="block">
                         <a href="https://akaunting.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
                             <img src="/www/assets/sponsors/akaunting.svg" class="w-auto h-5" />
-                        </a>
-                    </li>
-                    <li class="md:block hidden w-full h-0 -mt-12"></li>
-                    <li class="block">
-                        <a href="https://codecourse.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
-                            <img src="/www/assets/sponsors/codecourse.svg" class="w-auto h-5" />
                         </a>
                     </li>
                 </ul>
