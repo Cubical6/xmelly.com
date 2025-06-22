0ff830fe9013a28f11cd1a11bcc81af15d094496
Nuno Maduro <enunomaduro@gmail.com>
2025-06-22 12:20:46 +0100

    wip

--- resources/views/components/docs/sponsors.blade.php
+++ resources/views/components/docs/sponsors.blade.php
@@ -74,6 +74,12 @@
                     </li>
                     <li class="md:block hidden w-full h-0 -mt-12"></li>
                     <li class="block">
+                        <a href="https://devtools-for-livewire.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
+                            <img src="/www/assets/sponsors/devtools-for-livewire.svg" class="w-auto h-5" />
+                        </a>
+                    </li>
+                    <li class="md:block hidden w-full h-0 -mt-12"></li>
+                    <li class="block">
                         <a href="https://loadforge.com/?ref=pestphp" target="_blank" rel="noopener" class="table">
                             <img src="/www/assets/sponsors/loadforge.svg" class="w-auto h-5" />
                         </a>
