bceaa1aa5184dcafcbd416346f8758a29ca1bf98
Nuno Guerra <nunoandre360@gmail.com>
2025-08-20 20:21:54 +0100

    wip

--- resources/www/main.js
+++ resources/www/main.js
@@ -1,6 +1,13 @@
 import './main.css'
 
 import Alpine from 'alpinejs'
+import tippy from 'tippy.js'
+
+document.addEventListener('alpine:init', () => {
+    Alpine.directive('tooltip', (el, { expression }) => {
+        tippy(el, { content: expression });
+    });
+});
 
 window.Alpine = Alpine
 Alpine.start()
